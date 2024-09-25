e80a924e86d0dfcec8cf9ec9feaf9e87e8c80628
Abdelrhman Nashaat <abdelrhman.nashaat22@gmail.com>
2024-09-25 16:07:39 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+void main() async {
+  await dotenv.load(fileName: ".env");
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: GoogleMapView(),
+    );
+  }
+}
+
+class GoogleMapView extends StatefulWidget {
+  const GoogleMapView({super.key});
+
+  @override
+  State<GoogleMapView> createState() => _GoogleMapViewState();
+}
+
+class _GoogleMapViewState extends State<GoogleMapView> {
+  late CameraPosition initialCameraPosition;
+  @override
+  void initState() {
+    initialCameraPosition = const CameraPosition(target: LatLng(0, 0));
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GoogleMap(
+      zoomControlsEnabled: false,
+      initialCameraPosition: initialCameraPosition,
+    );
+  }
+}
